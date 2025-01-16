0ab233fe45e36ef9c5cfb0a3dc822765cad4fbbd
ShiwaniCC <shiwanisoni29082002@gmail.com>
2025-01-16 23:23:32 +0530

    Questions and answers list added

---
+++ lib/data/questionList.dart
@@ -0,0 +1,94 @@
+import 'package:quiz/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions(
+    'What has to be broken before you can use it?',
+    [
+      'Egg',
+      'Wall',
+      'Bottle',
+      'Door',
+    ],
+  ),
+  QuizQuestions(
+    'What is full of holes but still holds water?',
+    [
+      'Sponge',
+      'Bucket',
+      'Bottle',
+      'Sieve',
+    ],
+  ),
+  QuizQuestions(
+    'What question can you never answer yes to?',
+    [
+      'Are you asleep yet?',
+      'Are you hungry?',
+      'Do you know me?',
+      'Can you hear me?',
+    ],
+  ),
+  QuizQuestions(
+    'What is always in front of you but can’t be seen?',
+    [
+      'The future',
+      'Your shadow',
+      'The air',
+      'A mirror',
+    ],
+  ),
+  QuizQuestions(
+    'There’s a one-story house in which everything is yellow. Yellow walls, yellow doors, yellow furniture. What color are the stairs?',
+    [
+      'There are no stairs',
+      'Yellow',
+      'White',
+      'Brown',
+    ],
+  ),
+  QuizQuestions(
+    'What can you break, even if you never pick it up or touch it?',
+    [
+      'A promise',
+      'A glass',
+      'A stick',
+      'A secret',
+    ],
+  ),
+  QuizQuestions(
+    'I have branches, but no trunk, leaves, or fruit. What am I?',
+    [
+      'A bank',
+      'A tree',
+      'A library',
+      'A river',
+    ],
+  ),
+  QuizQuestions(
+    'The more of me you take, the more you leave behind. What am I?',
+    [
+      'Footsteps',
+      'Memories',
+      'Time',
+      'Knowledge',
+    ],
+  ),
+  QuizQuestions(
+    'I’m tall when I’m young, and I’m short when I’m old. What am I?',
+    [
+      'Candle',
+      'Tree',
+      'Pencil',
+      'Shadow',
+    ],
+  ),
+  QuizQuestions(
+    'What month of the year has 28 days?',
+    [
+      'All of them',
+      'December',
+      'February',
+      'None',
+    ],
+  )
+];
